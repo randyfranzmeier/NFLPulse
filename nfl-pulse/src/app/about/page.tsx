c0fcad20fbe6bd9666250f73244caf5aa4c22e1b
randyfranzmeier <Randyfranzmeier@gmail.com>
2025-06-20 13:20:56 -0500

    Customizing home page with navbar

---
+++ nfl-pulse/src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+export default function AboutPage() {
+    // TODO Implement this page using a clean, responsive design
+    return (
+        <div>
+            <h1> Welcome to the about page!</h1>
+        </div>
+    )
+}
